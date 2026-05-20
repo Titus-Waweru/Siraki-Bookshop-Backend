d7f8a8d58e1748e64166138a8e4f0d96fc27dda7
Titus Ngari Waweru <168157567+Titus-Waweru@users.noreply.github.com>
2026-05-20 09:32:52 +0300

    Fix CORS configuration for frontend origins

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://siraki-bookshop-frontend.vercel.app'
+    'https://siraki-bookshop-frontend.vercel.app',
     'https://www.sirakibookstore.co.ke'
   ]
 }));
